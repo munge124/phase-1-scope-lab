7570cd23e665dfb07ef4dab2409cd4d6bb432b68
benson <bensonmunge545@gmail.com>
2025-03-08 08:44:38 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+// Global variable
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer to 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// Global constant
+const leastFavoriteCustomer = 'John';
+
+// Function to try changing leastFavoriteCustomer (will throw error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane';  // This will throw an error because leastFavoriteCustomer is a constant
+}
+
